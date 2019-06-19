c50fd5dfc81fa9ddd0a4764c9cb116c62f6dc343
Drew <littonandrew@gmail.com>
2019-06-19 12:22:23 -0500

    Add project files.

---
+++ inClassExamples/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace inClassExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Drew";
+            char firstLetteroffirstname = 'D';
+            int favoriteNumber = 2;
+            double pi = 3.14;
+            bool isMarried = false;
+
+                  
+          
+         
+        }
+    }
+}
